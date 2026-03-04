3122f9247c59e6b9fa4b79ac8208770b77894210
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2026-03-04 19:28:55 -0300

    Initial Commit

---
+++ DailyChange/DailyChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyChangeApp.swift
+//  DailyChange
+//
+//  Created by Luis Filipe Pedroso on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
